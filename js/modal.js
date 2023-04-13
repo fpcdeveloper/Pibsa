ea97ff49efe718bfa26ff668cd74407fb2f5d98e
fpcdeveloper <fpc.developer@gmail.com>
2023-04-12 23:25:50 -0300

    12042023_2325

---
+++ js/modal.js
@@ -0,0 +1,57 @@
+// Ventana modal
+var modal1 = document.getElementById("ventanaModal_1");
+var modal3 = document.getElementById("ventanaModal_3");
+var modal4 = document.getElementById("ventanaModal_4");
+var modal5 = document.getElementById("ventanaModal_5");
+
+// Botón que abre el modal
+var boton1 = document.getElementById('abrirModal_1');
+var boton3 = document.getElementById('abrirModal_3');
+var boton4 = document.getElementById('abrirModal_4');
+var boton5 = document.getElementById('abrirModal_5');
+
+// Hace referencia al elemento <span> que tiene la X que cierra la ventana
+//var span = document.getElementsByClassName("cerrarModal_1")[0];
+
+// Cuando el usuario hace click en el botón, se abre la ventana
+boton1.addEventListener("click",function() {
+    modal1.style.display = "block";
+});
+boton3.addEventListener("click",function() {
+    modal3.style.display = "block";
+});
+boton4.addEventListener("click",function() {
+    modal4.style.display = "block";
+});
+boton5.addEventListener("click",function() {
+    modal5.style.display = "block";
+});
+
+// Si el usuario hace click en la x, la ventana se cierra
+/*span.addEventListener("click",function() {
+    modal.style.display = "none";
+});*/
+
+// Si el usuario hace click fuera de la ventana, se cierra.
+window.addEventListener("click",function(event) {
+if (event.target == modal1) {
+    modal1.style.display = "none";
+}
+});
+
+window.addEventListener("click",function(event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+});
+
+window.addEventListener("click",function(event) {
+    if (event.target == modal4) {
+        modal4.style.display = "none";
+    }
+});
+window.addEventListener("click",function(event) {
+    if (event.target == modal5) {
+        modal5.style.display = "none";
+    }
+});
--- js/pibsa.js
+++ js/pibsa.js
@@ -12,15 +12,50 @@
 
 
   // PRODUCTOS
-  
+
+   
   function inicioProductos(){
+      document.getElementById("cuadrado-1").style.display = "block"; 
+      document.getElementById('link-std').style.display = 'block';
+      document.getElementById('texto-link-std').style.display = 'none';  
+      document.getElementById("link-standard-naturales").style.display = "none";  
+      document.getElementById("link-standard-tecnicos").style.display = "none";  
       document.getElementById("productos_standard_1").style.display = "none";    
       document.getElementById("productos_standard_2").style.display = "none";  
+      document.getElementById("cuadrado-2").style.display = "block"; 
+      document.getElementById('link-gind').style.display = 'block';
+      document.getElementById('texto-link-gind').style.display = 'none';  
+      document.getElementById("link-garantia-naturales").style.display = "none";  
+      document.getElementById("link-garantia-tecnicos").style.display = "none";  
       document.getElementById("productos_garantia_1").style.display = "none";  
-      document.getElementById("productos_garantia_2").style.display = "none";              
-      document.getElementById("cuadrado-1").style.display = "block"; 
-      document.getElementById("cuadrado-2").style.display = "block";  
+      document.getElementById("productos_garantia_2").style.display = "none";
+      
   }
+
+  function moverLinkStd() {
+      document.getElementById('link-std').style.display = 'none';
+      document.getElementById('texto-link-std').style.display = 'block';                    
+  }
+
+  function mostrarSubtitulosStandard() {
+      document.getElementById('link-standard-naturales').style.display = 'block';
+      document.getElementById("link-standard-naturales").style.animation = "fade_in_show 1.2s";
+      document.getElementById('link-standard-tecnicos').style.display = 'block'; 
+      document.getElementById('link-standard-tecnicos').style.display = 'fade_in_show 1.2s'; 
+  }
+
+  function moverLinkGInd() {
+      document.getElementById('link-gind').style.display = 'none';
+      document.getElementById('texto-link-gind').style.display = 'block';                    
+  }
+
+  function mostrarSubtitulosGarantia() {
+    document.getElementById("link-garantia-naturales").style.display = "block";  
+    document.getElementById("link-garantia-naturales").style.animation = "fade_in_show 1.2s";
+    document.getElementById("link-garantia-tecnicos").style.display = "block";  
+    document.getElementById("link-garantia-tecnicos").style.animation = "fade_in_show 1.6s";
+  }
+
 
   function mostrarStandardNaturales(){
       document.getElementById("productos_standard_1").style.display = "block";
